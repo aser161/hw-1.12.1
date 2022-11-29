15033ee3525a38075260282d855b432d27ee401f
aser161 <aser161@yandex.ru>
2022-11-29 22:44:47 +0300

    Добавил поле title

--- src/Book.java
+++ src/Book.java
@@ -1,10 +1,15 @@
 public class Book {
+    private String title;
     private Author author;
     private int yearOfPublishing;
-    public Book (Author author, int yearOfPublishing){
+    public Book (String title, Author author, int yearOfPublishing){
+        this.title = title;
         this.author = author;
         this.yearOfPublishing = yearOfPublishing;
+    }
 
+    public String getTitle() {
+        return title;
     }
 
     public String getAuthor() {
--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ public class Main {
         Author APushkin = new Author("Александр Пушкин ");
         Author Hugo = new Author("Виктор Гюго ");
 
-        Book Dubrovskiy = new Book(APushkin, 1841);
-        Book Otvergennie = new Book(Hugo, 1862);
+        Book Dubrovskiy = new Book("Дубрровский ",APushkin, 1841);
+        Book Otvergennie = new Book("Отверженные ",Hugo, 1862);
 
         Dubrovskiy.setYearOfPublishing(1880);
         System.out.println(Dubrovskiy.getAuthor());
-        System.out.println("Дубрровский " + Dubrovskiy.getYearOfPublishing());
+        System.out.println(Dubrovskiy.getTitle() + Dubrovskiy.getYearOfPublishing());
         System.out.println();
 
         Otvergennie.setYearOfPublishing(1881);
         System.out.println(Otvergennie.getAuthor());
-        System.out.println("Отверженные " + Otvergennie.getYearOfPublishing());
+        System.out.println(Otvergennie.getTitle() + Otvergennie.getYearOfPublishing());
     }
 }
